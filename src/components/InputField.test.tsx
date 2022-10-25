cc0f9294cac181afba9e75bf49d647978fc557a2
Greg Ziebold <ziebold@gmail.com>
2022-10-25 15:57:54 -0600

    more InputField tests, add semi to linter

--- src/components/InputField.test.tsx
+++ src/components/InputField.test.tsx
@@ -4,15 +4,15 @@ import userEvent from "@testing-library/user-event"
 import "@testing-library/jest-dom"
 import InputField from "./InputField"
 
+const mockAdd = jest.fn()
+
+const setup = () => {
+  render(<InputField handleAdd={mockAdd} />)
+}
+
 test("render", async () => {
   // ARRANGE
-  render(
-    <InputField
-      handleAdd={() => {
-        return null
-      }}
-    />
-  )
+  setup()
 
   // ACT
   await screen.findByRole("textbox")
@@ -21,4 +21,31 @@ test("render", async () => {
   // ASSERT
   expect(screen.getByRole("textbox")).toHaveValue("")
   expect(screen.getByRole("button")).toBeEnabled()
+})
+
+test("input text", async () => {
+  // arrange
+  setup()
+  const input = await screen.findByRole("textbox")
+
+  // act
+  userEvent.type(input, "This is my task")
+
+  // assert
+  expect(input).toHaveValue("This is my task")
+})
+
+test("submit", async () => {
+  // arrange
+  setup()
+  const input = await screen.findByRole("textbox")
+  const button = await screen.findByRole("button")
+
+  // act
+  userEvent.type(input, "My task")
+  userEvent.click(button)
+
+  // assert
+  expect(mockAdd).toHaveBeenCalled()
+  expect(input).toHaveValue("")
 })
